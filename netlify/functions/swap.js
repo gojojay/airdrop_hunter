8d9076ce7ce7a6a98e1fa33b0c0cee0a099285fa
gojojay <132179442+gojojay@users.noreply.github.com>
2023-05-10 13:40:44 +0000

    swap.js

--- netlify/functions/swap.js
+++ netlify/functions/swap.js
@@ -1,35 +1,48 @@
 const { schedule } = require("@netlify/functions");
-const { ethers } = require("ethers")
-require("dotenv").config()
+const { ethers } = require("ethers");
+const zksync = require("zksync");
+require("dotenv").config();
 
-const IUniswapV2Router02 = require('@uniswap/v2-periphery/build/IUniswapV2Router02.json')
+const SyncswapRouterABI = require("./SyncswapRouterABI.json");
 
 const handler = async function (event, context) {
   console.log("Received event:", event);
 
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 
-  const ACCOUNT_1 = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider)
+  const ACCOUNT_1 = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider);
 
-  const V2_ROUTER = new ethers.Contract(process.env.V2_ROUTER_ADDRESS, IUniswapV2Router02.abi)
+  const syncswapRouterAddress = "0x2da10A1e27bF85cEdD8FFb1AbBe97e53391C0295"; // Replace this with the Syncswap router contract address
+  const SYNC_SWAP_ROUTER = new ethers.Contract(
+    syncswapRouterAddress,
+    SyncswapRouterABI,
+    provider
+  );
 
-  const PATH = [process.env.WETH_ADDRESS, process.env.TOKEN_ADDRESS]
-  const DEADLINE = Math.floor(Date.now() / 1000) + 60 * 10 // 10 minutes
+  const PATH = [process.env.WETH_ADDRESS, process.env.TOKEN_ADDRESS];
+  const DEADLINE = Math.floor(Date.now() / 1000) + 60 * 10; // 10 minutes
 
-  const transaction = await V2_ROUTER.connect(ACCOUNT_1).swapExactETHForTokens(
+  const transaction = await SYNC_SWAP_ROUTER.connect(ACCOUNT_1).swapExactETHForTokens(
     0,
     PATH,
     ACCOUNT_1.address,
     DEADLINE,
     { value: process.env.SWAP_AMOUNT }
-  )
+  );
 
-  console.log(`Swap Complete!`)
-  console.log(`See transaction at: https://goerli.etherscan.io/tx/${transaction.hash}/`)
+  console.log(`Swap Complete!`);
+
+  // zkSync part
+  const syncProvider = await zksync.getDefaultProvider("mainnet");
+  const syncWallet = await zksync.Wallet.fromEthSigner(ACCOUNT_1, syncProvider);
+
+  const zksyncTx = await syncProvider.getTransactionReceipt(transaction.hash);
+
+  console.log(`See transaction at: https://zksync2-mainnet.zkscan.io/explorer/transactions/${zksyncTx.hash}`);
 
   return {
     statusCode: 200,
-  }
-}
+  };
+};
 
-exports.handler = schedule("@daily", handler)
+exports.handler = schedule("@weekly", handler);
